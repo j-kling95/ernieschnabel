41bc17584f2f3cd78d0e4f2aafb57daed0d2ddc9
j-kling95 <julian.kling@mail-me.net>
2024-09-09 16:16:27 +0200

    Removed most of the methods in World.

--- src/Ernieschnabel.java
+++ src/Ernieschnabel.java
@@ -2,7 +2,6 @@ public class Ernieschnabel {
     public static void main(String[] args) {
 
        World world = new World(3);
-       Platypus platypus = new Platypus("Ernie", true);
 
     }
 }
--- src/World.java
+++ src/World.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 
 public class World {
 
-    private ArrayList<Cheese> food;
-    private ArrayList<Fields> fields;
-
-
-
     //The world should be generated in 3 sizes: small, medium, large;
     //Small: 25 fields, Medium: 64 Fields, Large: 100 Fields;
     //Depending on the size, an ID system is drawn that assigns coordinates.
@@ -20,60 +15,11 @@ public class World {
 
 
     public World(int amount){
-        food = new ArrayList<>();
-        fields = new ArrayList<>();
-//        generateCheese(amount);
+
     }
 
     public void generateWorld(){
 
-       while (true){
-           Scanner reader1 = new Scanner(System.in);
-           System.out.println("Select a world size! 1 = small (6x6), 2 = medium (8x8), 3 = large (12x12)");
-           int size = reader1.nextInt();
-
-           if (size == 1){
-               //generate 36 fields
-               for (int i = 0; i < 5 ; i++) {
-
-                   }
-
-               }
-               break;
-           }
-
-           if (size == 2){
-               //generate 64 fields
-               break;
-           }
-
-           if (size ==3){
-              //generate 64 fields
-               break;
-          }
-           else {
-               System.out.println("You can only select a world size between 1 and 3.");
-               continue;
-
-
-           }
-       }
     }
-//   public void generateCheese(int amount){
-//       for (int i = 0; i <= amount ; i++) {
-//           Scanner reader1 = new Scanner(System.in);
-//           System.out.println("Enter a taste: ");
-//           int taste = reader1.nextInt();
-//
-//
-//           Scanner reader2 = new Scanner(System.in);
-//           System.out.println("Enter a name: ");
-//           String name = reader2.nextLine();
-//
-//           Cheese cheese = new Cheese(taste, name);
-//           food.add(cheese);
-//
-//       }
-//   }
 
 }
